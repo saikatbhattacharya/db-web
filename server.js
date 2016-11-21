20e7ebc648d89a3674c779774a838e1e360ddbf5
saikatcts <Saikat.Bhattacharya5@cognizant.com>
2016-11-21 08:17:49 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  if(!req.query.content) res.sendFile(__dirname +'/index.html')
+  else res.sendFile(__dirname +'/content.html');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});

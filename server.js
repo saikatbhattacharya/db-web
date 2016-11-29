76a5d83a84cd9ff4294a84d23bac297d16024046
saikatcts <Saikat.Bhattacharya5@cognizant.com>
2016-11-29 09:02:07 +0530

    new resources

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
   else res.sendFile(__dirname +'/content.html');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
